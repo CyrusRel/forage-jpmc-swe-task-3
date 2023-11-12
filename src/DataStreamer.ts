f66ca36d7f66b9bbfd4db7fe5f552a830ebfac3a
CyrusRel <bryanhernandez_48@outlook.com>
2023-11-11 19:30:32 -0600

    before adding historical gathering

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -12,6 +12,7 @@ export interface ServerRespond {
 class DataStreamer {
   static API_URL: string = 'http://localhost:8080/query?id=1';
 
+
   static getData(callback: (data: ServerRespond[]) => void): void {
     const request = new XMLHttpRequest();
     request.open('GET', DataStreamer.API_URL, false);
@@ -26,6 +27,9 @@ class DataStreamer {
 
     request.send();
   }
+
+
+
 }
 
 export default DataStreamer;
